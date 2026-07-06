adc27b7d06ef9268d9a916ac1bcaa5cf0ff0ca73
francescanovelli <novellifrancescaa@gmail.com>
2026-07-06 14:17:26 +0200

    Update RAO.r

--- RAO.r
+++ RAO.r
@@ -1,1 +1,21 @@
+library(dplyr)
 
+rao_q_fun <- function(z) {
+  n <- length(z)
+  if (n < 2) return(NA_real_)
+  
+  dmat <- as.matrix(dist(z))
+  p <- rep(1/n, n)
+  
+  as.numeric(t(p) %*% dmat %*% p)
+}
+
+rao_ant <- ant %>%
+  group_by(plot_id) %>%
+  summarise(
+    n_trees = n(),
+    rao_zmax = rao_q_fun(zmax),
+    .groups = "drop"
+  )
+
+rao_ant
